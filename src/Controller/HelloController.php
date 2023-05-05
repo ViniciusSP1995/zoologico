a177a662575f3d915dd7fae3395115680acf175b
Vinicius <vds1995sp@gmail.com>
2023-05-05 15:37:35 -0300

    Twig adicionando no projeto

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 
 class HelloController
@@ -14,51 +15,13 @@ class HelloController
         return new Response('Olá, mundo!');
     }
 
-    #[Route('/dinossauro')]
-    public function dinossauro(): Response
+    #[Route('/animal/{slug}')]
+    public function animal(string $slug=null): Response
     {
-        return new Response('Olá, dinossauro!');
+        $newSlug = str_replace('-',' ', $slug);
+        $newSlug = u($newSlug)->title(true);
+        return new Response(content: 'Olá, ' . $newSlug);
     }
 
-    #[Route('/mamifero')]
-    public function mamifero(): Response
-    {
-        return new Response('Olá, mamifero!');
-    }
-
-    #[Route('/invertebrados')]
-    public function invertebrados(): Response
-    {
-        return new Response('Olá, invertebrados!');
-    }
-
-    #[Route('/insetos')]
-    public function insetos(): Response
-    {
-        return new Response('Olá, insetos!');
-    }
-
-    #[Route('/aves')]
-    public function aves(): Response
-    {
-        return new Response('Olá, aves!');
-    }
-
-    #[Route('/felinos')]
-    public function felinos(): Response
-    {
-        return new Response('Olá, felinos!');
-    }
-
-    #[Route('/caninos')]
-    public function caninos(): Response
-    {
-        return new Response('Olá, caninos!');
-    }
-
-    #[Route('/roedores')]
-    public function roedores(): Response
-    {
-        return new Response('Olá, roedores!');
-    }
+   
 }
